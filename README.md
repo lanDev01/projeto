df686a237b3577fcc61f48362b6fe1728913180a
Alan <lnjnr9215@gmail.com>
2023-05-16 09:29:22 -0300

    initializing project

---
+++ README.md
@@ -0,0 +1,4 @@
+### `npm start`
+ 
+
+ <!-- bootstrap utilizado com link cdn css/js -->
---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+      <nav className="navbar navbar-dark bg-dark">
+        <a className="navbar-brand text-uppercase" href="/">Rick And Morty</a>
+      </nav>
+  );
+}
+
+export default App;
